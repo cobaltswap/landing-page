47155de2f49d771d7ee57ffb2195522aaa4b9c8e
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2022-10-10 09:41:35 +0100

    ADD: Add sitemap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
